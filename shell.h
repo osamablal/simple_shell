0b4976d9608d8a75fe211216387dc3c0a63079ce
osamablal <osamablal2013@gmail.com>
2023-08-23 02:46:42 -0700

    updateed

--- shell.h
+++ shell.h
@@ -92,5 +92,65 @@ char *our_memcpy(char *z, char *v, unsigned int n);
 void *our_realloc(void *, unsigned int, unsigned int);
 void *our_calloc(unsigned int nmb, unsigned int siz);
 
+#ifndef _INC_SIGNAL
+#define _INC_SIGNAL
+
+#include <crtdefs.h>
+#include <pthread_signal.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef _SIG_ATOMIC_T_DEFINED
+#define _SIG_ATOMIC_T_DEFINED
+  typedef int sig_atomic_t;
+#endif
+
+#define NSIG 23
+
+#define SIGINT 2
+#define SIGILL 4
+#define SIGABRT_COMPAT 6
+#define SIGFPE 8
+#define SIGSEGV 11
+#define SIGTERM 15
+#define SIGBREAK 21
+#define SIGABRT 22
+#define SIGABRT2 22
+
+#ifdef _POSIX
+#define	SIGHUP	1
+#define	SIGQUIT	3
+#define	SIGTRAP	5
+#define SIGIOT  6
+#define	SIGEMT	7
+#define	SIGKILL	9
+#define	SIGBUS	10
+#define	SIGSYS	12
+#define	SIGPIPE	13
+#ifdef __USE_MINGW_ALARM
+#define	SIGALRM	14
+#endif
+#endif
+
+  typedef	void (*__p_sig_fn_t)(int);
+
+#define SIG_DFL (__p_sig_fn_t)0
+#define SIG_IGN (__p_sig_fn_t)1
+#define SIG_GET (__p_sig_fn_t)2
+#define SIG_SGE (__p_sig_fn_t)3
+#define SIG_ACK (__p_sig_fn_t)4
+#define SIG_ERR (__p_sig_fn_t)-1
+
+  extern void **__cdecl __pxcptinfoptrs(void);
+#define _pxcptinfoptrs (*__pxcptinfoptrs())
+
+  __p_sig_fn_t __cdecl signal(int _SigNum,__p_sig_fn_t _Func);
+  int __cdecl raise(int _SigNum);
+
+#ifdef __cplusplus
+}
+
 #endif
 
